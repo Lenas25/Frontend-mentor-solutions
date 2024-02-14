3d28997d97212574e4f0c0f1af033a01d0df2389
Lena <102402197+Lenas25@users.noreply.github.com>
2024-02-14 18:24:41 -0500

    feat: finish and add js for menu

---
+++ loopstudios-landing/script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector('body'),
+    ul = body.querySelector('ul'),
+    // para obtener la lista que va a desplegarse
+    btn = body.querySelector('.menu'),
+    // para obtener el boton que despliega la lista
+    icono= body.querySelector('.bx-menu');
+
+  btn.addEventListener('click', () => {
+    // al hacer click en el boton
+    ul.classList.toggle('active');
+    // se agrega o se quita la clase active a la lista
+    icono.classList.toggle('bx-x');
+  });
